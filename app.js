720fdbd69826fc0661452e432c59b150d1211779
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-03-09 14:15:31 +0100

    Html get works

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname +'/views/test.html');
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
