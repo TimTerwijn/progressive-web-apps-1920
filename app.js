40fdf67939c368cb2324a74b2b37294df27ac237
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-03-09 14:20:12 +0100

    ejs works

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-/* GET home page. */
-app.get('/', function(req, res, next) {
-  res.sendFile(__dirname +'/views/test.html');
-});
+// Link the templating engine to the express app
+app.set('view engine', 'ejs');
+// Tell the views engine/ejs where the template files are stored (Settingname, value)
+app.set('views', 'views');
 
+// Tell express to use a 'static' folder
+// If the url matches a file it will send that file
+// Sending something (responding) ends the response cycle
+app.use(express.static('public'));
+
+/* GET home page. */
+app.get('/', function(req, res) {
+  res.render('test');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
