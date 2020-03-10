7c838c7ea96ef08b8b8dd2256d4d80c0c428efa6
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-03-10 12:43:26 +0100

    refector mark2

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,25 @@ import {checkKey} from "./modules/controller.js";
 import * as render from "./modules/render.js";
 
 export function onLeftKey(){
+  //render animation
   render.walkLeft();  
+
+  //callback to server
+
 }
 
 export function onRightKey(){
+  //render animation
   render.walkRight(); 
+
+  //callback to server
+  
 }
 
 export function onTopKey(){
+  //render animation
   render.playerJump();
+
+  //callback to server
+  
 }
