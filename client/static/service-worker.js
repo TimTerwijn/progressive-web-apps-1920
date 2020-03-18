02a23beef1e12b986a5f985bfc348ee1c056949a
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-03-18 08:28:15 +0100

    Service worker works

--- client/static/service-worker.js
+++ client/static/service-worker.js
@@ -1,69 +1,39 @@
-var CACHE = 'network-or-cache';
+let cacheName = 'cache-v1';
 var urlsToCache = [
-'/',
-'/Dieren',
-'/Vakanties',
-'/Boeken',
-'/Stoer',
-'/css/style.css',
-'/img/marco_l.png',
-'/img/marco_r.png',
-'/js/app.js',
-'/js/modules/controller.js',
-'/js/modules/register.js',
-'/js/modules/render.js',
-'/js/modules/vars.js'
-];
+  '/',
+  '/Dieren',
+  '/Vakanties',
+  '/Boeken',
+  '/Stoer',
+  '/css/style.css',
+  '/img/marco_l.png',
+  '/img/marco_r.png',
+  '/js/app.js',
+  '/js/modules/controller.js',
+  '/js/modules/register.js',
+  '/js/modules/render.js',
+  '/js/modules/vars.js'
+  ];
 
-// On install, cache some resource.
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
+self.addEventListener('install', function (event) {
 
-  // Ask the service worker to keep installing until the returning promise
-  // resolves.
-  evt.waitUntil(precache());
+  const cache = caches.open(cacheName).then(function (_cache){
+    _cache.addAll(urlsToCache);
+  });
+
+  event.waitUntil(cache);
 });
 
-// On fetch, use cache but update the entry with the latest contents
-// from the server.
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  // Try network and if it fails, go for the cached copy.
-  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
-    return fromCache(evt.request);
-  }));
+//inspired by https://stackoverflow.com/a/51948641
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open(cacheName).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function (response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
-
-// Open a cache and use `addAll()` with an array of assets to add all of them
-// to the cache. Return a promise resolving when all the assets are added.
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(urlsToCache);
-  });
-}
-
-// Time limited network request. If the network fails or the response is not
-// served before timeout, the promise is rejected.
-function fromNetwork(request, timeout) {
-  return new Promise(function (fulfill, reject) {
-    // Reject in case of timeout.
-    var timeoutId = setTimeout(reject, timeout);
-    // Fulfill in case of success.
-    fetch(request).then(function (response) {
-      clearTimeout(timeoutId);
-      fulfill(response);
-    // Reject also if network fetch rejects.
-    }, reject);
-  });
-}
-
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
